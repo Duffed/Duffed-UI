00d8588806e53f361ea9d2c08e4b53d74a70ff78
Duffed <duffed90@gmx.de>
2011-06-30 17:11:47 +0200

    5.53 - includes Tukui 13.24 bug fixes (guess its important idk)

--- Tukui/modules/blizzard/kill.lua
+++ Tukui/modules/blizzard/kill.lua
@@ -13,6 +13,8 @@ Kill:SetScript("OnEvent", function(self, event, addon)
 			InterfaceOptionsFrameCategoriesButton11:SetAlpha(0)
 			CompactRaidFrameManager:Kill()
 			CompactRaidFrameContainer:Kill()
+			CompactUnitFrame_UpateVisible = T.dummy
+			CompactUnitFrame_UpdateAll = T.dummy
 			SetCVar("useCompactPartyFrames", 1)
 		end	
 	else
